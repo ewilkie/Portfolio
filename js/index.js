904c72a7629aa5ee7249363cf5dc668d7df54467
ewilkie <emilie.wilkie@gmail.com>
2023-02-05 19:25:04 +1100

    mobile slide in menu functionality

--- js/index.js
+++ js/index.js
@@ -3,9 +3,26 @@ let menu = document.querySelector('.menu');
 
 function toggleMenu(e) {
     toggleMenuBtn.classList.toggle('open');
-    menu.classList.toggle('open');  
+    menu.classList.toggle('open'); 
 }
 
 toggleMenuBtn.addEventListener('click', toggleMenu);
 
+
+
+
+let menuLinks = document.querySelectorAll('.menu-link');
+
+for (let i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    setTimeout(function() {
+      window.location.href = menuLinks[i].getAttribute("href");
+    }, 500);
+
+    toggleMenuBtn.classList.toggle('open');
+    menu.classList.toggle('open'); 
+
+  });
+}
 
