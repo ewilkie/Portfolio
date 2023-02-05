febc550b060fbfee204b9d568d3d56c0eb550b97
ewilkie <emilie.wilkie@gmail.com>
2023-02-05 19:30:19 +1100

    comments

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+/* controls hamburger menu and slide out menu */ 
 let toggleMenuBtn = document.querySelector('.menu-button');
 let menu = document.querySelector('.menu');
 
@@ -10,15 +12,18 @@ toggleMenuBtn.addEventListener('click', toggleMenu);
 
 
 
-
+/* functionality for navigating to different sections */
 let menuLinks = document.querySelectorAll('.menu-link');
 
 for (let i = 0; i < menuLinks.length; i++) {
+    
   menuLinks[i].addEventListener('click', function(event) {
+    /*
     event.preventDefault();
     setTimeout(function() {
       window.location.href = menuLinks[i].getAttribute("href");
     }, 500);
+    */
 
     toggleMenuBtn.classList.toggle('open');
     menu.classList.toggle('open'); 
