71922cf3bf3364385ed97b7bcc2ec99133746f6a
ewilkie <emilie.wilkie@gmail.com>
2023-02-06 10:29:59 +1100

    fixed menu-link click resize bug

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-/* controls hamburger menu and slide out menu */ 
+// controls hamburger menu and slide out menu 
 let toggleMenuBtn = document.querySelector('.menu-button');
 let menu = document.querySelector('.menu');
 
@@ -12,22 +12,22 @@ toggleMenuBtn.addEventListener('click', toggleMenu);
 
 
 
-/* functionality for navigating to different sections */
+// functionality for navigating to different sections 
 let menuLinks = document.querySelectorAll('.menu-link');
 
 for (let i = 0; i < menuLinks.length; i++) {
     
   menuLinks[i].addEventListener('click', function(event) {
-    /*
-    event.preventDefault();
-    setTimeout(function() {
-      window.location.href = menuLinks[i].getAttribute("href");
-    }, 500);
-    */
+    
+    /* delay only applies to mobile menu */
+    if (window.innerWidth <= 600) {
+        event.preventDefault();
+        setTimeout(function() {
+        window.location.href = menuLinks[i].getAttribute("href");
+        }, 500);
 
-    toggleMenuBtn.classList.toggle('open');
-    menu.classList.toggle('open'); 
-
+        toggleMenuBtn.classList.toggle('open');
+        menu.classList.toggle('open'); 
+    }
   });
 }
-
